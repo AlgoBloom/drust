0c4f560ed39dbfb50738e63f70d50bcbd450fdb2
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-03 17:36:50 -0400

    Completed section 1.3. Hello, Cargo!

---
+++ projects/hello_world/target/debug/deps/hello_cargo-12c687c84e48e3ac.d
@@ -0,0 +1,5 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/hello_world/target/debug/deps/hello_cargo-12c687c84e48e3ac.rmeta: src/main.rs
+
+/home/bloomdev/RUST-WORKSPACE/drust/projects/hello_world/target/debug/deps/hello_cargo-12c687c84e48e3ac.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_world/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/hello_world/target/debug/hello_cargo: /home/bloomdev/RUST-WORKSPACE/drust/projects/hello_world/src/main.rs
