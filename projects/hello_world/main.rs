d56320c38eaadb540ca8abd0225d6c8cb5f7c655
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-03 17:22:27 -0400

    Completed 1.2. Hello, World!

--- projects/hello_world/main.rs
+++ projects/hello_world/main.rs
@@ -1,3 +1,10 @@
+// a function names main is defined
+// main function is special because it is the first function that runs in any reach program
+// this function has no parameters and returns nothing
+// parameters would go inside the parentheses
+// we use curly brackets in rust to contain a function
 fn main() {
+    // rust indents four spaces not a tab
+    // println! calls a macro, you know its a macro because of !
     println!("Hello, world!");
 }
