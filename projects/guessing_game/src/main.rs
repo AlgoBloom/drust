c061413a53a32d8df21d20d63e8269745b624400
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-03 18:47:13 -0400

    Added comments while reading through handling potential failure with the Result type.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -13,13 +13,31 @@ fn main() {
 
     println!("Please input your guess.");
 
-    // varible mul for storing user input
+    // varible guess for storing user input
     // use let statement to create the variable
     // in rust vars are immutible by default so use let to make sure this variable is mutable
+    // mut adds a type of mutable to this variable making it mutable
+    // this variable returns a new string
+    // allows multiple parts of your code to access one piece of data without needing to copy that data into memory multiple times
     let mut guess = String::new();
 
+    // calling stdin function allows us to handle user input
     io::stdin()
+        // calls the read line method
+        // passing the guess variable into the read line method
+        // this allows us to tell guess what to store
+        // takes user input and appends it to a string
+        // & indicates that this argument is a reference
+        // must continue using mut to clearly define the variable as mutable
+        // read line returns a Result value of type enum, or enumeration, which is a type that can be in one of multiple possible states, we call each possible state a variant
+        // purpose of Result types is to encode error handling information
         .read_line(&mut guess)
+        // the Result's variants are Ok and Err
+        // Ok variant indicates the operation was successful and inside Ok is the successfully generated value
+        // Err variant means the operation failed and Err contains information about how or why the operation failed
+        // values or the Result type, like values of any type, have methods defined on them
+        // an instance of Result has an expect method that you can call
+        // we use except becuase we want to crash the program when a problem occurs
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
