92c855846a5eeefca77f021047530fae42f107e9
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-03 21:51:19 -0400

    Added loop to allow user to guess multiple times.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -24,52 +24,57 @@ fn main() {
     // then we call the gen_range method on the random number generator
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    // printing the secret number
-    println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        // printing the secret number
+        println!("The secret number is: {secret_number}");
 
-    // varible guess for storing user input
-    // use let statement to create the variable
-    // in rust vars are immutible by default so use let to make sure this variable is mutable
-    // mut adds a type of mutable to this variable making it mutable
-    // this variable returns a new string
-    // allows multiple parts of your code to access one piece of data without needing to copy that data into memory multiple times
-    let mut guess = String::new();
+        println!("Please input your guess.");
 
-    // calling stdin function allows us to handle user input
-    io::stdin()
-        // calls the read line method
-        // passing the guess variable into the read line method
-        // this allows us to tell guess what to store
-        // takes user input and appends it to a string
-        // & indicates that this argument is a reference
-        // must continue using mut to clearly define the variable as mutable
-        // read line returns a Result value of type enum, or enumeration, which is a type that can be in one of multiple possible states, we call each possible state a variant
-        // purpose of Result types is to encode error handling information
-        .read_line(&mut guess)
-        // the Result's variants are Ok and Err
-        // Ok variant indicates the operation was successful and inside Ok is the successfully generated value
-        // Err variant means the operation failed and Err contains information about how or why the operation failed
-        // values or the Result type, like values of any type, have methods defined on them
-        // an instance of Result has an expect method that you can call
-        // we use except becuase we want to crash the program when a problem occurs
-        .expect("Failed to read line");
+        // varible guess for storing user input
+        // use let statement to create the variable
+        // in rust vars are immutible by default so use let to make sure this variable is mutable
+        // mut adds a type of mutable to this variable making it mutable
+        // this variable returns a new string
+        // allows multiple parts of your code to access one piece of data without needing to copy that data into memory multiple times
+        let mut guess = String::new();
 
-    // shadowing in rust lets us reuse the guess variable name rather than forcing us to create two unique variables
-    // often used when you want to convert data types
-    // bind new variable to the expression guess.trim().parse()
-    // trims the whitespace and parse converts type
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // calling stdin function allows us to handle user input
+        io::stdin()
+            // calls the read line method
+            // passing the guess variable into the read line method
+            // this allows us to tell guess what to store
+            // takes user input and appends it to a string
+            // & indicates that this argument is a reference
+            // must continue using mut to clearly define the variable as mutable
+            // read line returns a Result value of type enum, or enumeration, which is a type that can be in one of multiple possible states, we call each possible state a variant
+            // purpose of Result types is to encode error handling information
+            .read_line(&mut guess)
+            // the Result's variants are Ok and Err
+            // Ok variant indicates the operation was successful and inside Ok is the successfully generated value
+            // Err variant means the operation failed and Err contains information about how or why the operation failed
+            // values or the Result type, like values of any type, have methods defined on them
+            // an instance of Result has an expect method that you can call
+            // we use except becuase we want to crash the program when a problem occurs
+            .expect("Failed to read line");
 
-    // this line prints the string that now contains the user's input
-    println!("You guessed: {guess}");
-    
-    // this functionality figured out if the user guess matches the random number
-    // a match expression is made up of arms, an arm consists of a pattern to match against, and the code that should be run if the value fiven to match fits the arm's pattern
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // shadowing in rust lets us reuse the guess variable name rather than forcing us to create two unique variables
+        // often used when you want to convert data types
+        // bind new variable to the expression guess.trim().parse()
+        // trims the whitespace and parse converts type
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        // this line prints the string that now contains the user's input
+        println!("You guessed: {guess}");
+        
+        // this functionality figured out if the user guess matches the random number
+        // a match expression is made up of arms, an arm consists of a pattern to match against, and the code that should be run if the value fiven to match fits the arm's pattern
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+
     }
-}
+
+}    
