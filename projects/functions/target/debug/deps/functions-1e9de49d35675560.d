a3997348b8914c37bbf4a3713dadff49bd9c5cac
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-04 22:47:13 -0400

    Added functions project.

---
+++ projects/functions/target/debug/deps/functions-1e9de49d35675560.d
@@ -0,0 +1,5 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/functions/target/debug/deps/functions-1e9de49d35675560.rmeta: src/main.rs
+
+/home/bloomdev/RUST-WORKSPACE/drust/projects/functions/target/debug/deps/functions-1e9de49d35675560.d: src/main.rs
+
+src/main.rs:
---
+++ projects/functions/target/debug/deps/functions-93a35c859604858e.d
@@ -0,0 +1,5 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/functions/target/debug/deps/functions-93a35c859604858e.rmeta: src/main.rs
+
+/home/bloomdev/RUST-WORKSPACE/drust/projects/functions/target/debug/deps/functions-93a35c859604858e.d: src/main.rs
+
+src/main.rs:
