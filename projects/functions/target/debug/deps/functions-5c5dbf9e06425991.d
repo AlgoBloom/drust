93618520e00e5f21cd43376d54ba6e5a6b527786
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-04 22:52:29 -0400

    Added parameter for another_function.

---
+++ projects/functions/target/debug/deps/functions-5c5dbf9e06425991.d
@@ -0,0 +1,5 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/functions/target/debug/deps/functions-5c5dbf9e06425991: src/main.rs
+
+/home/bloomdev/RUST-WORKSPACE/drust/projects/functions/target/debug/deps/functions-5c5dbf9e06425991.d: src/main.rs
+
+src/main.rs:
---
+++ projects/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/functions/target/debug/functions: /home/bloomdev/RUST-WORKSPACE/drust/projects/functions/src/main.rs
