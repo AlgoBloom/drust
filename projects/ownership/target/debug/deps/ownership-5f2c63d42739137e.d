e7a35e86f503e09965853f2235e186dc3f7fe410
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-05 21:45:59 -0400

    Added string literal hello saved as a variable called s.

---
+++ projects/ownership/target/debug/deps/ownership-5f2c63d42739137e.d
@@ -0,0 +1,5 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/ownership/target/debug/deps/ownership-5f2c63d42739137e.rmeta: src/main.rs
+
+/home/bloomdev/RUST-WORKSPACE/drust/projects/ownership/target/debug/deps/ownership-5f2c63d42739137e.d: src/main.rs
+
+src/main.rs:
---
+++ projects/ownership/target/debug/deps/ownership-f0f5f2f287c8d676.d
@@ -0,0 +1,5 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/ownership/target/debug/deps/ownership-f0f5f2f287c8d676.rmeta: src/main.rs
+
+/home/bloomdev/RUST-WORKSPACE/drust/projects/ownership/target/debug/deps/ownership-f0f5f2f287c8d676.d: src/main.rs
+
+src/main.rs:
