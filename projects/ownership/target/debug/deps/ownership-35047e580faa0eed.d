e2ff781a3cd6d678f0ee048a973de3cc53f8190a
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-06 11:19:33 -0400

    Added a clone method to perform a deep copy of the heap data for the hello string.

---
+++ projects/ownership/target/debug/deps/ownership-35047e580faa0eed.d
@@ -0,0 +1,5 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/ownership/target/debug/deps/ownership-35047e580faa0eed: src/main.rs
+
+/home/bloomdev/RUST-WORKSPACE/drust/projects/ownership/target/debug/deps/ownership-35047e580faa0eed.d: src/main.rs
+
+src/main.rs:
---
+++ projects/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/bloomdev/RUST-WORKSPACE/drust/projects/ownership/target/debug/ownership: /home/bloomdev/RUST-WORKSPACE/drust/projects/ownership/src/main.rs
