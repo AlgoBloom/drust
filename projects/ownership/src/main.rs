052963a46a053c0d025d16027688c3d9b3a3537b
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-06 21:08:57 -0400

    Added error dangle

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,11 +1,7 @@
-fn main() {
-    let mut s = String::from("hello");
+fn dangle() -> &String { // dangle returns a reference to a String
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{} and {}", r1, r2);
-    // variables r1 and r2 will not be used after this point
+    let s = String::from("hello"); // s is a new String
 
-    let r3 = &mut s; // no problem
-    println!("{}", r3);
-}
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
