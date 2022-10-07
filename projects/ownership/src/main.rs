2955ff812ae37a6a0516f03f9acbcfed978d944d
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-06 21:11:05 -0400

    Changed dangling function to not be a reference so that it does not cause an error and returns the string as a reference to the heap.

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -2,6 +2,5 @@ fn dangle() -> &String { // dangle returns a reference to a String
 
     let s = String::from("hello"); // s is a new String
 
-    &s // we return a reference to the String, s
-} // Here, s goes out of scope, and is dropped. Its memory goes away.
-  // Danger!
+    s 
+} 
