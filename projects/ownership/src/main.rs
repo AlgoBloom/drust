bc868636d990c2888e758c870376a5590da42de4
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-08 15:10:59 -0400

    Changed main file to use a byte slice.

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,14 +1,12 @@
 fn main() {
     let mut s = String::from("hello world");
 
-    let word = first_word(&s); // word will get the value 5
+    let word = first_word(&s);
 
-    s.clear(); // this empties the String, making it equal to ""
+    s.clear(); // error!
 
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally invalid!
+    println!("the first word is: {}", word);
 }
-
 fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
